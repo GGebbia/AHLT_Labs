5db5c9814681ec275625dc2c2eb6d47e41e568ee
milla95 <>
2020-02-27 22:48:40 +0100

    Work in progress...

--- Lab_1/baseline-NER.py
+++ Lab_1/baseline-NER.py
@@ -14,39 +14,70 @@ nltk.download('punkt')
 
 stopWords = set(stopwords.words('english'))
 
+
 # Replace punctuation symbols by space in order to maintain the offset.
 def remove_punctuation(sentence):
     sentence = re.sub(r'\([^)]*\)', ' ', sentence)
     sentence = re.sub("?!", " ", sentence)
     return sentence
 
+
 def tokenize(sentence):
     span_generator = WhitespaceTokenizer().span_tokenize(sentence)
-    return [(sentence[span[0]: span[1]], span[0], span[1]-1) for span in span_generator]
+    return [(sentence[span[0]: span[1]], span[0], span[1] - 1) for span in span_generator]
+
+
+def strip_word_end(word):
+    punctuation = [",", ":", ";", ")", "!", "?"]
+    if any(word.endswith(punct) for punct in punctuation):
+        word = word[:-1]
+        return True, word
+    else:
+        return False, word
+
 
 # TODO afegir knowledge de english words (the, in etc. + drugs i brands que sabem de test)
 # TODO mirar com detectar brands
-# TODO les vitamines posa "vitamine D" i detectem D com a drug, mirar si paraula anterior es vitamine i actual és una lletra i no contar com a drug
+
 def extract_entities(tokenized_list):
     entities_list = []
     last_type = None
     word_stripped = False
-    list_length = len(tokenized_list)-1
+    list_length = len(tokenized_list) - 1
+    # we use skip word when we want to store next token (vitamine D),
+    # when we reach vitamine, we take D also, so when we take the next token (D), we skip it
+    skip_word = False
+    global suffixes_list
+
     for i, token in enumerate(tokenized_list):
         word, offset_from, offset_to = token
         d = {}
 
+        if skip_word:
+            skip_word = False
+            continue
+
+        if word.lower() in stopWords:
+            last_type = None
+            continue
+
         # remove . from last word
         if i == list_length and word.endswith("."):
             word = word[:-1]
-        # strip , and : from word
-        elif any(punct in word for punct in punctuation):
-            print(word)
-            word = word[:-1]
-            word_stripped = True
+            offset_to -= 1
+            # this should only happen if last word is a single ".""
+        if not word:
+            continue
 
-        # this should only happen if last word is a single ".""
-        if not word or len(word)<=2:
+        # strip symbols from end of the word
+        continue_stripping_word = True
+
+        while continue_stripping_word:
+            continue_stripping_word, word = strip_word_end(word)
+            if continue_stripping_word:
+                offset_to-= 1
+                word_stripped = True
+        if not word:
             continue
 
         # If word contains any of the suffixes in the list, then mark it as drug
@@ -57,22 +88,30 @@ def extract_entities(tokenized_list):
             last_type = "drug"
 
         # If word is vitamin, we store with the second letter
-        if word == "vitamin":
-            post_word, _, post_offset_to = tokenized_list[i+1]
+        elif word == "vitamin":
+            post_word, _, post_offset_to = tokenized_list[i + 1]
+            continue_stripping_word = True
+            while continue_stripping_word:
+                continue_stripping_word, post_word = strip_word_end(post_word)
+                if continue_stripping_word:
+                    post_offset_to -= 1
+                    word_stripped = True
             d["name"] = "{} {}".format(word, post_word)
             d["type"] = "drug"
             d["offset"] = "{}-{}".format(offset_from, post_offset_to)
             last_type = "drug"
+            skip_word = True
 
-        if (word[0].isupper() and offset_from != 0) or (word.isupper()):
+        # todo canviar a brand o droga depenent de sufix
+        elif (word[0].isupper() and offset_from != 0) or (word.isupper()):
             d["name"] = word
-            d["type"] = "drug" # Posar pes per fer probabilitat 2/3 si es drug, 1/3 si es brand
+            d["type"] = "brand"  # Posar pes per fer probabilitat 2/3 si es drug, 1/3 si es brand
             d["offset"] = "{}-{}".format(offset_from, offset_to)
             last_type = "drug"
 
-        #TODO: si la paraula seguent tambe te el mateix tipus, merge
+        # TODO: si la paraula seguent tambe te el mateix tipus, merge
         elif word.lower() == "acid":
-            prev_word, prev_offset_from, _ = tokenized_list[i-1]
+            prev_word, prev_offset_from, _ = tokenized_list[i - 1]
 
             # Remove drug or brand if it was added since the next word is acid
             if last_type != None:
@@ -93,20 +132,23 @@ def extract_entities(tokenized_list):
         if d.keys(): entities_list.append(d)
     return entities_list
 
+
 def output_entities(sid, entities, outputfile):
     for ent in entities:
         line = '|'.join([sid, ent["offset"], ent["name"], ent["type"]])
         outputfile.write(line + "\n")
 
+
 def evaluate(inputdir, outputfile):
-    os.system("java -jar eval/evaluateNER.jar "+ inputdir + " " + outputfile)
+    os.system("java -jar eval/evaluateNER.jar " + inputdir + " " + outputfile)
+
 
 ### VARIABLES
 inputdir = sys.argv[1]
-outputfilename="./output.txt"
+outputfilename = "./task9.1_GianMarc_1.txt"
 outputfile = open(outputfilename, "w")
-suffixes_list = [line.strip() for line in open("sufixes_devel.txt","r")]
-punctuation = [",", ":", ";", ")", "!", "?"]
+suffixes_list = [line.strip() for line in open("sufixes_devel.txt", "r")]
+
 for filename in os.listdir(inputdir):
     file_path = os.path.join(inputdir, filename)
     tree = ET.parse(file_path)
@@ -118,6 +160,5 @@ for filename in os.listdir(inputdir):
         tokenized_list = tokenize(sentence)
         entities = extract_entities(tokenized_list)
         output_entities(sid, entities, outputfile)
-
 evaluate(inputdir, outputfilename)
 outputfile.close()
