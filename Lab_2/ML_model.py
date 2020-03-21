75587b207a2718ba1b7129666ac199471e86cc29
milla95 <>
2020-03-21 14:10:53 +0100

    lab2 0.57 F1 without knowledge

--- Lab_2/ML_model.py
+++ Lab_2/ML_model.py
@@ -4,13 +4,14 @@ from itertools import chain
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.preprocessing import LabelBinarizer
 
+
 def split_data(word_features):
     X_samples = []
     sample = []
-    
+
     Y_labels = []
     label = []
-    
+
     for word_feature in word_features:
         if word_feature != "":
             word_feature = word_feature.split(" ")
@@ -24,26 +25,49 @@ def split_data(word_features):
             label = []
     return X_samples, Y_labels
 
+
 def output_predicted_entities(Y_pred, filename):
     Y_pred_flatten = [el for line in Y_pred for el in line]
     detailed_word_features = open(filename).read().split("\n")
     detailed_word_features = [el for el in detailed_word_features if el != ""]
-    
-    for label, word_feature in zip(Y_pred_flatten,detailed_word_features):
+
+    skip_next_word = False
+    skip_next_word_times = 0
+    for index, (label, word_feature) in enumerate(zip(Y_pred_flatten, detailed_word_features)):
+        if skip_next_word:
+            skip_next_word_times -= skip_next_word_times
+            if skip_next_word_times == 0:
+                skip_next_word = False
+            continue
+
         if label == "O":
             continue
         else:
             label = label.split("-")[1]
             _id, word, offset_from, offset_to = word_feature.split(" ")[:4]
+
+            # merge i-type with b-type
+            next_word_index = 1
+            while("I-" == Y_pred_flatten[index + next_word_index][0:2]):
+                # todo ajuntar mes de un word
+                _, new_word, _, new_offset_to = detailed_word_features[index+1].split(" ")[:4]
+                offset_to = new_offset_to
+                word = word + " " + new_word
+                skip_next_word = True
+                skip_next_word_times += skip_next_word_times
+                next_word_index += 1
+
             line = [_id, "{}-{}".format(offset_from, offset_to), word, label]
             outputfile.write("|".join(line) + "\n")
 
-def evaluate(inputdir, outputfile): 
-	os.system("java -jar eval/evaluateNER.jar " + inputdir + " " + outputfile)         
+
+def evaluate(inputdir, outputfile):
+    os.system("java -jar eval/evaluateNER.jar " + inputdir + " " + outputfile)
+
 
 ### MAIN VARIABLES
 train_filename = "data_Train_megam.dat"
-#test_filename = "data_Devel_megam.dat"
+# test_filename = "data_Devel_megam.dat"
 test_filename = "data_Devel_megam.dat"
 
 fulltest_filename = "data_Devel_feats.dat"
@@ -62,9 +86,9 @@ for xseq, yseq in zip(X_train, Y_train):
     trainer.append(xseq, yseq)
 
 trainer.set_params({
-    'c1': 1.0,   # coefficient for L1 penalty
+    'c1': 1.0,  # coefficient for L1 penalty
     'c2': 1e-3,  # coefficient for L2 penalty
-    'max_iterations': 50,  # stop earlier
+    'max_iterations': 250,  # stop earlier #todo put higher value
 
     # include transitions that are possible, but not observed
     'feature.possible_transitions': True
@@ -82,4 +106,3 @@ output_predicted_entities(Y_pred, fulltest_filename)
 outputfile.close()
 
 evaluate(inputdir, output_filename)
-
--- Lab_2/feature_extactor.py
+++ Lab_2/feature_extactor.py
@@ -8,8 +8,8 @@ import os
 import sys
 import re
 import argparse
-nltk.download('punkt')
 
+nltk.download('punkt')
 
 
 class Token:
@@ -17,7 +17,7 @@ class Token:
         self.word = word
         self.offset_from = int(offset_from)
         self.offset_to = int(offset_to)
-        self.type = "O"                                # Initialize all tokens with type O as non-drugs non-brands and non-groups
+        self.type = "O"  # Initialize all tokens with type O as non-drugs non-brands and non-groups
 
     def word_iscapitalized(self):
         return self.word[0].isupper()
@@ -25,8 +25,6 @@ class Token:
     def word_isupper(self):
         return self.word.isupper()
 
-    def word_hassuffix(self):
-        return any(suffix in self.word for suffix in suffixes_list)
 
 class Entity:
 
@@ -55,6 +53,7 @@ class Entity:
 
 
 # Read a string sentence and tokenize it by words with the corresponding offset. words ending with comma or dot are splitted onto two tokenized words. Return a tokenized list of sentence.
+
 def tokenize(sentence):
     span_generator = WhitespaceTokenizer().span_tokenize(sentence)
     tokens = [(sentence[span[0]: span[1]], span[0], span[1] - 1) for span in span_generator]
@@ -62,7 +61,8 @@ def tokenize(sentence):
     new_tokens = []
     for i, token in enumerate(tokens):
         word, offset_from, offset_to = token
-        if (len(word) > 1) and ( word.endswith(',') or word.endswith('.') ):
+
+        if (len(word) > 1) and (word.endswith(',') or word.endswith('.') or word.endswith(':') or word.endswith(';')):
             punct = word[-1]
             punct_offset_from = offset_to
             punct_offset_to = offset_to
@@ -72,14 +72,28 @@ def tokenize(sentence):
 
             new_tokens.append(Token(word, offset_from, offset_to))
             new_tokens.append(Token(punct, punct_offset_from, punct_offset_to))
+
+        elif (len(word) > 1) and word[0] == '(' and (word[0:2] != '(+' or word[0:2] != '(-'):
+            punct = word[0]
+            punct_offset_from = offset_from
+            punct_offset_to = offset_from
+
+            word = word[1:]
+            offset_from += 1
+
+            new_tokens.append(Token(punct, punct_offset_from, punct_offset_to))
+            new_tokens.append(Token(word, offset_from, offset_to))
         else:
             new_tokens.append(Token(word, offset_from, offset_to))
 
     return new_tokens
 
+
 # Given a token list, collect the most relevant features to store in a list.
 def extract_features(tokens):
     features = []
+
+    all_words_capitalized = all(token.word_iscapitalized() or len(token.word) < 2 for token in tokens)
 
     for i, token in enumerate(tokens, 0):
         word_features = []
@@ -89,15 +103,34 @@ def extract_features(tokens):
         word_features.append("form={}".format(form))
         suf4 = token.word[-4:]
         word_features.append("suf4={}".format(suf4))
+        max_pref_len = 4
+        if len(token.word) < max_pref_len:
+            max_pref_len = len(token.word)
+        pref4 = token.word[0:max_pref_len]
+        word_features.append("pref4={}".format(pref4))
         if i != 0:
-            prev = tokens[i-1].word
+            prev = tokens[i - 1].word
+            word_features.append("prevsuf4={}".format(prev[-4:]))
+            max_pref_len = 4
+            if len(prev) < max_pref_len:
+                max_pref_len = len(prev)
+            prevpref4 = prev[0:max_pref_len]
+            word_features.append("prevpref4={}".format(prevpref4))
+
         else:
             prev = "_BoS_"
         word_features.append("prev={}".format(prev))
 
+        # todo try next and previous prefix and sufix
         try:
-            _next = tokens[i+1].word
+            _next = tokens[i + 1].word
             word_features.append("next={}".format(_next))
+            max_pref_len = 4
+            if len(_next) < max_pref_len:
+                max_pref_len = len(_next)
+            nextpref4 = _next[0:max_pref_len]
+            word_features.append("nextpref4={}".format(nextpref4))
+            word_features.append("nextsuf4={}".format(_next[-4:]))
         except:
             pass
 
@@ -108,25 +141,23 @@ def extract_features(tokens):
         if token.word_isupper():
             word_features.append("isupper")
 
-        if token.word_hassuffix():
-            word_features.append("drugsuffix")
+        if all_words_capitalized:
+            word_features.append("all_capitalized")
+
+        if token.word
 
         features.append(word_features)
 
     return features
 
+
 def detect_label(token, entities):
-
     for entity in entities:
-        print(token.offset_from, token.offset_to, token.word)
-        print(entity.offset_from, entity.offset_to, entity.word)
-        print("\n")
-
         # If the two offsets are equal, then it corresponds to the same word and type.
         if token.offset_from == entity.offset_from and token.offset_to == entity.offset_to:
             token.type = "B-" + entity.type
             return
-# If the token offset interval is inside the entity offset interval, then it is a first or the continuation of a type sequence.
+        # If the token offset interval is inside the entity offset interval, then it is a first or the continuation of a type sequence.
         elif entity.offset_from <= token.offset_from and token.offset_to <= entity.offset_to:
             if entity.offset_from == token.offset_from:
                 token.type = "B-" + entity.type
@@ -137,13 +168,12 @@ def detect_label(token, entities):
         else:
             token.type = "O"
 
+
 # Write the feature extraction together with the label class and offsets into a file to future
 def output_features(sid, tokens, entities, features, flag="feats"):
-
-
     if flag == "feats":
 
-        for i,token in enumerate(tokens):
+        for i, token in enumerate(tokens):
             detect_label(token, entities)
             line = [sid, token.word, str(token.offset_from), str(token.offset_to), token.type] + features[i]
             outputfile.write(" ".join(line) + "\n")
@@ -158,34 +188,34 @@ def output_features(sid, tokens, entities, features, flag="feats"):
         print("Incorrect feature extraction type\n")
         sys.exit(1)
 
+
 # Store as a list of dictionaries the word, the offset interval and the label (drug, group, brand, drug_n,...) of each entity in the sentence.
 def get_entities(child):
     entities = []
     for ent in child.findall('entity'):
         entity = Entity(**ent.attrib)
         entities.append(entity)
-        print(ent.attrib)
     return entities
+
 
 ### MAIN ###
 
 parser = argparse.ArgumentParser(description=
-        """
+                                 """
         Compute the feature extractor of a given dataset containing XML Files.\n
         Usage: \n\npython3 feature_extractor.py --dir data/Train --type <feats|megam>\n\n
         If feats is selected returns the complete feature extractor with more detailed information about each sample.\n
         If megam is selected returns only the features of each sample.\n
         """
-        )
-parser.add_argument('--type', type=str, choices=["feats","megam"], help='Two option of extraction: feats or megam')
+                                 )
+parser.add_argument('--type', type=str, choices=["feats", "megam"], help='Two option of extraction: feats or megam')
 parser.add_argument('--dir', type=str, help='Directory where are located the XML files')
 
 args = parser.parse_args()
 
 inputdir = args.dir
-outputfilename = inputdir.replace("/","_") + "_%s.dat" % args.type
+outputfilename = inputdir.replace("/", "_") + "_%s.dat" % args.type
 outputfile = open(outputfilename, "w")
-
 
 # INTERNAL KNOWLEDGE
 suffixes_list = [line.strip() for line in open("sufixes_no_knowledge.txt", "r")]
@@ -203,4 +233,3 @@ for filename in os.listdir(inputdir):
         entities = get_entities(child)
         output_features(sid, tokens, entities, features, flag=args.type)
 outputfile.close()
-
