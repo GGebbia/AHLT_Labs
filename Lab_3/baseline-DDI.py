ee508021ee89bd7a7de77d6d0ff84c75f0c96a25
milla95 <>
2020-04-09 16:46:30 +0200

    All results above 0.15: - Train 0.22 - Devel 0.4173 - Test 0.1873
    Todo: improve effect type, is very low. Check order in which we classify. Comment code properly. Check other improvements?

--- Lab_3/baseline-DDI.py
+++ Lab_3/baseline-DDI.py
@@ -16,8 +16,7 @@ def analyze(s):
     last_offset_end = 0
     for head_node in mytree:
         for key in sorted(head_node.nodes, key=lambda key: int(key)):
-
-            # first parsed is no word
+            # first key is not first word (is root)
             if key == 0:
                 continue
             # find first occurrence of substring token in sentence
@@ -29,13 +28,96 @@ def analyze(s):
                 word = ")"
 
             offset_start = s.find(word, last_offset_end)
-            offset_end = offset_start + len(word) - 1 # -1 as length 1 is same start and end
+            offset_end = offset_start + len(word) - 1  # -1 as length 1 is same start and end
             # store last offsets
             last_offset_end = offset_end
             # add start and end to the token
             head_node.nodes[key]['start'] = offset_start
             head_node.nodes[key]['end'] = offset_end
-    return mytree
+
+    return head_node
+
+
+def get_entity_node_key(entity, analysis):
+    for key in sorted(analysis.nodes, key=lambda key: int(key)):
+        try:
+            if analysis.nodes[key]['start'] == int(entity[0]):
+                return key
+        except KeyError:
+            pass
+    return 0
+
+
+def isNodeInParent(parent, word):
+    for node in parent:
+        if type(node) is nltk.Tree:
+            if node.label() == word:
+                return True
+            if isNodeInParent(node, word):
+                return True
+        else:
+            if node == word:
+                return True
+    return False
+
+
+# receives DependencyGraph with all sentence, list of entities and the ids of the 2 entities to be checked
+def check_interaction(analysis, entities, e1, e2):
+    result = "0"
+    interaction = "null"
+
+    e1_node_key = get_entity_node_key(entities[e1], analysis)
+    e2_node_key = get_entity_node_key(entities[e2], analysis)
+    e1_word = analysis.nodes[e1_node_key]['word']
+    e2_word = analysis.nodes[e2_node_key]['word']
+
+    # if not found assume no interaction
+    if e1_node_key == 0 or e2_node_key == 0:
+        return result, interaction
+
+    for key in sorted(analysis.nodes, key=lambda key: int(key)):
+        try:
+            current_word = analysis.nodes[key]['word']
+            tree = analysis.tree()
+            if current_word in int_clue_words:
+                for subtree in tree.subtrees():
+                    if subtree.label() in int_clue_words:
+                        if isNodeInParent(subtree, e1_word) and isNodeInParent(subtree, e2_word):
+                            result = "1"
+                            interaction = "int"
+
+            elif current_word in mechanism_clue_words:
+                for subtree in tree.subtrees():
+                    if subtree.label() in mechanism_clue_words:
+                        if isNodeInParent(subtree, e1_word) and isNodeInParent(subtree, e2_word):
+                            result = "1"
+                            interaction = "mechanism"
+
+            elif current_word in effect_clue_words:
+                for subtree in tree.subtrees():
+                    if subtree.label() in effect_clue_words:
+                        if isNodeInParent(subtree, e1_word) and isNodeInParent(subtree, e2_word):
+                            result = "1"
+                            interaction = "effect"
+
+            elif current_word in advise_clue_words:
+                next_word = analysis.nodes[key + 1]['word']
+                if next_word == "not" and analysis.nodes[key + 2]['word'] == "be" \
+                        and analysis.nodes[key + 3]['tag'] == "VBN" \
+                        or next_word == "be" and analysis.nodes[key + 2]['tag'] == "VBN":
+
+                    result = "1"
+                    interaction = "advise"
+
+        except KeyError:
+            pass
+
+    return result, interaction
+
+
+# receives data dir and filename for the results to evaluate
+def evaluate(inputdir, outputfile):
+    os.system("java -jar eval/evaluateDDI.jar " + inputdir + " " + outputfile)
 
 
 #### VARIABLES ####
@@ -43,10 +125,18 @@ inputdir = sys.argv[1]
 outputfilename = "./task9.2_TrainGianMarc_1.txt"
 outputfile = open(outputfilename, "w")
 
-# connect to CoreNLP server 
+#### RULE VARIABLES ####
+effect_clue_words = {"administer", "potentiate", "prevent"}
+mechanism_clue_words = {"reduce", "increase", "decrease"}
+int_clue_words = {"interact", "interaction"}
+advise_clue_words = {"may", "might", "should"}
+
+# connect to CoreNLP server
 my_parser = CoreNLPDependencyParser(url="http://localhost:9000")
 
 #### MAIN ####
+
+# TODO mirar ordre!
 
 # process each file in directory
 for filename in os.listdir(inputdir):
@@ -75,11 +165,9 @@ for filename in os.listdir(inputdir):
         for pair in pairs:
             id_e1 = pair.attrib["e1"]
             id_e2 = pair.attrib["e2"]
-            # TODO check_interaction function
-            # (is_ddi, ddi_type) = check_interaction(analysis, entities, id_e1, id_e2)
-            line = "|".join([sid, id_e1, id_e2])  # is_ddi, ddi_type])
+            (is_ddi, ddi_type) = check_interaction(analysis, entities, id_e1, id_e2)
+            line = "|".join([sid, id_e1, id_e2, is_ddi, ddi_type])
             outputfile.write(line + "\n")
 
-#TODO define evaluate
-#evaluate(inputdir, outputfilename)
+evaluate(inputdir, outputfilename)
 outputfile.close()
