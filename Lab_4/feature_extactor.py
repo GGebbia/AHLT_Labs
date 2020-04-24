eb8bd018c1e4b32826deeee2216ae91c03d596e5
milla95 <>
2020-04-24 20:14:44 +0200

    Add features

--- Lab_4/feature_extactor.py
+++ Lab_4/feature_extactor.py
@@ -76,7 +76,6 @@ def is_node_in_parent(parent, word):
     return False
 
 def get_subtree_from_word(tree, ent):
-
     # Get the first word if there are more than one
     for word in ent.split():
         entity_subtree =  list(tree.subtrees(filter=lambda t: t.label() == word))
@@ -126,12 +125,139 @@ def get_heads_and_relations(entity_key, analysis):
     heads_rels = _get_heads_and_relations(entity_key, analysis)
     return heads_rels
 
+def count_no_not_in_sentence(sentence):
+    counter = 0
+    for word in nltk.word_tokenize(sentence):
+        if word == 'not' or word == 'no' or word[-3:] == "n't":
+            counter += 1
+        return counter
+
+def mechanism_words_present(sentence):
+    for word in nltk.word_tokenize(sentence):
+        if word in mechanism_clue_words:
+            return True
+    return False
+
+def effect_words_present(sentence):
+    for word in nltk.word_tokenize(sentence):
+        if word in effect_clue_words:
+            return True
+    return False
+
+def int_words_present(sentence):
+    for word in nltk.word_tokenize(sentence):
+        if word in int_clue_words:
+            return True
+    return False
+
+def special_words_count(sentence):
+    counter = 0
+    for word in nltk.word_tokenize(sentence.lower()):
+        if word in special_words:
+            counter += 1
+    return counter
+
+def special_words_pos_count(sentence, e1_offset_1, e1_offset_2, e2_offset_1, e2_offset_2):
+    counter = 0
+    first_phrase = sentence[0:e1_offset_1].split()
+    mid_phrase = sentence[e1_offset_2 + 1:e2_offset_1].split()
+    end_phrase = sentence[e2_offset_2 + 1:].split()
+    before_first_count = len(set(first_phrase) & special_words)
+    middle_count = len(set(mid_phrase) & special_words)
+    after_second_count = len(set(end_phrase) & special_words)
+    return before_first_count, middle_count, after_second_count
+
+def negative_words_count(sentence, e1_offset_1, e1_offset_2, e2_offset_1, e2_offset_2):
+    first_phrase = sentence[0:e1_offset_1].split()
+    mid_phrase = sentence[e1_offset_2 + 1:e2_offset_1].split()
+    end_phrase = sentence[e2_offset_2 + 1:].split()
+    before_first_count = len(set(first_phrase) & negative_words)
+    middle_count = len(set(mid_phrase) & negative_words)
+    after_second_count = len(set(end_phrase) & negative_words)
+    return before_first_count, middle_count, after_second_count
+
+def count_key_words_present(sentence):
+    key_words = ['coadministration', 'concomitant', 'concomitantly']
+    count = 0
+    for word in key_words:
+        if word in nltk.word_tokenize(sentence.lower()):
+            count += 1
+    return count
+
+def words_sep_by_and(sentence, e1_word, e2_word):
+    sentence = sentence.replace(e1_word, 'e1')
+    sentence = sentence.replace(e2_word, 'e2')
+    words = nltk.word_tokenize(sentence)
+    try:
+        if abs(words.index('e1') - words.index('e2')) < 3:
+            if words[words.index('e1') + 1] == 'and':
+                return True
+    except ValueError:
+        pass
+    return False
+
+def words_sep_by_comma(sentence, e1_word, e2_word):
+    sentence = sentence.replace(e1_word, 'e1')
+    sentence = sentence.replace(e2_word, 'e2')
+    words = nltk.word_tokenize(sentence)
+    try:
+        if abs(words.index('e1') - words.index('e2')) < 3:
+            if words[words.index('e1') + 1] == ',':
+                return True
+    except ValueError:
+        pass
+    return False
+
+def key_phrase(sentence, e1_word, e2_word):
+    sentence = sentence.replace(e1_word, 'drug1')
+    sentence = sentence.replace(e2_word, 'drug2')
+    key_phrases = ['concurrent administration of drug1 and drug2', 'drug1 concurrently with drug2',
+            'co-administration of drug1 and drug2', 'coadministration of drug1 and drug2',
+               'concurrent use of drug1 and drug2',]
+    for phrase in key_phrases:
+        if phrase in sentence.lower():
+            return True
+    return False
+
+def count_words_between(sentence, e1_offset_end, e2_offset_begin):
+    words_between = sentence[e1_offset_end+1:e2_offset_begin].split()
+    return len(words_between)
+
+
+
+#TODO !!!!!!!!! Funcio que compti numero de drugs entre les nostres entitats
+#TODO !!!!!!!!! Funcio que compti nombre de verbs abans de la primera entitat, nombre de verbs entre les dues i nombre de verbs al final!
+
+# TODO sub_obj :; '''binary feature that checks if drug1 is the nsubj or in its subtree
+#     and if drug2 is dobj or in its subtree, returns 1 if both, 0 otherwise''' USE the tree for this
+# TODO both_head check if both drug mentions have same head Use the tree for this
+# TODO is there a special_word in path between 2 entities
+
 # Given a token list, collect the most relevant features to store in a list.
 def extract_features(analysis, sentence, entities, e1, e2):
+    #TODO try: separated by comma, sseparated by "and"
+    #try concurrent phrases: phrases = ['concurrent administration of drug1 and drug2', 'drug1 concurrently with drug2',
+     #       'co-administration of drug1 and drug2', 'coadministration of drug1 and drug2',
+     #          'concurrent use of drug1 and drug2',]
+    #num words between drugs
+    # num of not and no in the sentence
+    #num drugs between drugs
+    # num of verbs before, middle, final
+    # '''binary feature that checks if drug1 is the nsubj or in its subtree
+    # and if drug2 is dobj or in its subtree, returns 1 if both, 0 otherwise'''
+    #check if have same head
+    #num of verbs in each segment
+
+    tree = analysis.tree()
+
     features = []
 
     key_e1 = get_entity_node_key(e1,analysis)
     key_e2 = get_entity_node_key(e2,analysis)
+
+    # get the corresponding word (token) of each entity from the dependency graph
+    e1_word = analysis.nodes[key_e1]['word']
+    e2_word = analysis.nodes[key_e2]['word']
 
     if key_e1 == 0 or key_e2 == 0:
         return ""
@@ -146,20 +272,74 @@ def extract_features(analysis, sentence, entities, e1, e2):
     for i, (head, rel) in enumerate(path_to_cp_e2):
         features.append("dep_e2_{}={}".format(i,rel))
 
-    tree = analysis.tree()
+    for key in sorted(analysis.nodes, key=lambda key: int(key)):
+        try:
+            # get current word of current node
+            current_word = analysis.nodes[key]['word']
+            # Check if interaction is advise
+            if current_word in advise_clue_words:
+                next_word = analysis.nodes[key + 1]['word']
+                if next_word == "not" and analysis.nodes[key + 2]['word'] == "be" \
+                        and analysis.nodes[key + 3]['tag'] == "VBN" \
+                        or next_word == "be" and analysis.nodes[key + 2]['tag'] == "VBN":
+                    features.append("advise_guess")
+        except KeyError:
+            pass
 
-   #  for lemma in sentence.partition(e1)[0].split():
-   #      features.append("lb1={}".format(lemma))
-   #  for lemma in find_between(sentence, e1, e2).split():
-   #      features.append("lib={}".format(lemma))
-   #  for lemma in sentence.partition(e2)[2].split():
-   #      features.append("la2={}".format(lemma))
+    # direct path cc
+
+    #  for lemma in sentence.partition(e1)[0].split():
+    #      features.append("lb1={}".format(lemma))
+    #  for lemma in find_between(sentence, e1, e2).split():
+    #      features.append("lib={}".format(lemma))
+    #  for lemma in sentence.partition(e2)[2].split():
+    #      features.append("la2={}".format(lemma))
 
     if key_e1 in [el[0] for el in heads_rels_e1]:
         features.append("1under2")
     elif  key_e2 in [el[0] for el in heads_rels_e2]:
         features.append("2under1")
 
+    count_nots = count_no_not_in_sentence(sentence)
+    # TODO, look that this feature (and all with = and a value) is correctly added, we want count_not to be a feature and the value to be the value,
+    # not a one hot for each possible count_not=X combination.
+    features.append("count_not={}".format(count_nots))
+    if mechanism_words_present(sentence):
+        features.append("mechanism_guess")
+    if effect_words_present(sentence):
+        features.append("effect_guess")
+    if int_words_present(sentence):
+        features.append("int_guess")
+
+    # TODO Look also this feature is a number in the table and not a one hot
+    special_w = special_words_count(sentence)
+    features.append("special_w={}".format(special_w))
+
+    # TODO look also this are values in the table and not one hot
+    special_w_pos_b, special_w_pos_mid, special_w_pos_end = special_words_pos_count(sentence, e1.offset_from, e1.offset_to, e2.offset_from, e2.offset_to)
+    features.append("special_w_pos_begin={}".format(special_w_pos_b))
+    features.append("special_w_pos_mid={}".format(special_w_pos_mid))
+    features.append("special_w_pos_end={}".format(special_w_pos_end))
+
+    # TODO look also this are values in the table and not one hot
+    neg_words_count_b, neg_words_count_mid, neg_words_count_end = negative_words_count(sentence, e1.offset_from, e1.offset_to, e2.offset_from, e2.offset_to)
+    features.append("neg_word_begin={}".format(neg_words_count_b))
+    features.append("neg_word_mid={}".format(neg_words_count_mid))
+    features.append("neg_word_end={}".format(neg_words_count_end))
+
+    # TODO look also this values are in the tbale and not one hot
+    key_words_count = count_key_words_present(sentence)
+    features.append("num_keywords={}".format(key_words_count))
+
+    # TODO look also this values are in the tbale and not one hot
+    words_between = count_words_between(sentence, e1.offset_to, e2.offset_from)
+    features.append("words_between={}".format(words_between))
+    if words_sep_by_and(sentence, e1_word, e2_word):
+        features.append("separated_by_and")
+    if words_sep_by_comma(sentence, e1_word, e2_word):
+        features.append("separated_by_comma")
+    if key_phrase(sentence, e1_word, e2_word):
+        features.append("key_phrase")
     # e1_tree = get_subtree_from_word(tree, e1)
     # e2_tree = get_subtree_from_word(tree, e2)
     # if is_node_in_parent(e1_tree, e2):
@@ -222,6 +402,21 @@ outputfile = open(outputfilename, "w")
 
 # connect to CoreNLP server
 my_parser = CoreNLPDependencyParser(url="http://localhost:9000")
+
+effect_clue_words = {"administer", "potentiate", "prevent", "antagonize", "antagonized"}
+mechanism_clue_words = {"reduce", "increase", "decrease"}
+int_clue_words = {"interact", "interaction"}
+advise_clue_words = {"may", "might", "should"}
+special_words = []
+with open("special_words.txt") as f:
+    for line in f:
+        special_words.append(line)
+special_words = set(special_words)
+negative_words = ['No', 'not', 'neither', 'without', 'lack', 'fail',
+                  'unable', 'abrogate', 'absence', 'prevent',
+                  'unlikely', 'unchanged', 'rarely']
+
+negative_words = set(negative_words)
 
 # process each file in directory
 for filename in os.listdir(inputdir):
